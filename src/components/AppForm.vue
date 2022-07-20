6b04b82b4ca62972a81ef3df83141bee6532f0d7
Vladislav Nozhkin <mega.nozhkin@pm.me>
2022-07-20 12:55:44 +0300

    deleted container

--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -1,39 +1,37 @@
 <template>
-  <div class="container">
-    <form class="form">
-      <custom-input
-        :label="'Наименование товара'"
-        :typeInput="'text'"
-        :placeholder="'Введите наименование товара'"
-        :id="'name'"
-        :isRquired="true"
-      />
+  <form class="form">
+    <custom-input
+      :label="'Наименование товара'"
+      :typeInput="'text'"
+      :placeholder="'Введите наименование товара'"
+      :id="'name'"
+      :isRquired="true"
+    />
 
-      <custom-textarea
-        :label="'Описание товара'"
-        :placeholder="'Введите описание товара'"
-        :id="'description'"
-      />
+    <custom-textarea
+      :label="'Описание товара'"
+      :placeholder="'Введите описание товара'"
+      :id="'description'"
+    />
 
-      <custom-input
-        :label="'Ссылка на изображение товара'"
-        :typeInput="'text'"
-        :placeholder="'Введите ссылку'"
-        :id="'linkImage'"
-        :isRquired="true"
-      />
+    <custom-input
+      :label="'Ссылка на изображение товара'"
+      :typeInput="'text'"
+      :placeholder="'Введите ссылку'"
+      :id="'linkImage'"
+      :isRquired="true"
+    />
 
-      <custom-input
-        :label="'Цена товара'"
-        :typeInput="'text'"
-        :placeholder="'Введите цену'"
-        :id="'price'"
-        :isRquired="true"
-      />
+    <custom-input
+      :label="'Цена товара'"
+      :typeInput="'text'"
+      :placeholder="'Введите цену'"
+      :id="'price'"
+      :isRquired="true"
+    />
 
-      <custom-button> Добавить товар </custom-button>
-    </form>
-  </div>
+    <custom-button> Добавить товар </custom-button>
+  </form>
 </template>
 
 <script>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="header">
-    <div class="container">
-      <div class="header__wrapper">
-        <h1 class="header__title">Добавление товара</h1>
+    <div class="header__wrapper">
+      <h1 class="header__title">Добавление товара</h1>
 
-        <select class="header__sort">
-          <option>По умолчанию</option>
-        </select>
-      </div>
+      <select class="header__sort">
+        <option>По умолчанию</option>
+      </select>
     </div>
   </div>
 </template>
