7e373421b1fca7aafff33edbb27e568cc52eca48
Vladislav Nozhkin <mega.nozhkin@pm.me>
2022-07-21 11:19:46 +0300

    deleted a modal window

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <template>
-  <div>
-    <div class="container">
-      <app-header />
-      <div class="app__wrapper">
-        <app-form />
-        <product-list />
-        <custom-modal-call-button @click="showModal" />
-      </div>
+  <div class="container">
+    <app-header />
+    <div class="app__wrapper">
+      <app-form />
+      <product-list />
     </div>
-
-    <modal-form v-if="isModal" @click="hideModal" />
   </div>
 </template>
 
@@ -17,34 +12,14 @@
 import AppHeader from "./components/AppHeader.vue";
 import AppForm from "./components/AppForm.vue";
 import ProductList from "./components/ProductList.vue";
-import CustomModalCallButton from "./components/UI/CustomModalCallButton.vue";
-import ModalForm from "./components/ModalForm.vue";
 
 export default {
   name: "App",
-
-  data() {
-    return {
-      isModal: false,
-    };
-  },
 
   components: {
     AppHeader,
     AppForm,
     ProductList,
-    CustomModalCallButton,
-    ModalForm,
-  },
-
-  methods: {
-    showModal() {
-      this.isModal = true;
-    },
-
-    hideModal() {
-      this.isModal = false;
-    },
   },
 };
 </script>
