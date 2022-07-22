fced4fa45100125f729e12bcdc44971f665f5fd6
Vladislav Nozhkin <mega.nozhkin@pm.me>
2022-07-22 14:15:01 +0300

    added delete animation

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="product__item" @click="removeProduct(product.id)">
+    <div class="product__img">
+      <img :src="product.img" :alt="product.title" />
+    </div>
+
+    <div class="product__content">
+      <h3 class="product__title">{{ product.title }}</h3>
+
+      <p class="product__description">
+        {{ product.description }}
+      </p>
+
+      <span class="product__price"> {{ product.price }} руб. </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductItem",
+
+  props: {
+    product: {
+      type: Object,
+      required: false,
+    },
+  },
+
+  emits: {
+    "remove-product": (product) =>
+      product ? product : console.error("Ошибка при удалении"),
+  },
+
+  methods: {
+    removeProduct(id) {
+      this.$emit("remove-product", id);
+    },
+  },
+};
+</script>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,48 +1,55 @@
 <template>
   <div class="product">
-    <div
-      class="product__item"
-      v-for="product in products"
-      :key="product.id"
-      @click="removeProduct(product.id)"
-    >
-      <div class="product__img">
-        <img :src="product.img" :alt="product.title" />
-      </div>
-
-      <div class="product__content">
-        <h3 class="product__title">{{ product.title }}</h3>
-
-        <p class="product__description">
-          {{ product.description }}
-        </p>
-
-        <span class="product__price"> {{ product.price }} руб. </span>
-      </div>
-    </div>
+    <transition-group name="product-list">
+      <product-item
+        v-for="product in products"
+        :key="product.id"
+        @remove-product="removeProduct"
+        :product="product"
+      />
+    </transition-group>
   </div>
 </template>
 
 <script>
+import ProductItem from "./ProductItem.vue";
 export default {
   name: "ProductList",
-
   props: {
     products: {
       type: Array,
       required: true,
     },
   },
-
   emits: {
     "remove-product": (product) =>
       product ? product : console.error("Ошибка при удалении"),
   },
-
   methods: {
     removeProduct(id) {
       this.$emit("remove-product", id);
     },
   },
+  components: { ProductItem },
 };
 </script>
+
+<style lang="scss" scoped>
+.product-list-enter-active {
+  animation: product-list-in 0.5s;
+}
+.product-list-leave-active {
+  animation: product-list-in 0.5s reverse;
+}
+@keyframes product-list-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.25);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
